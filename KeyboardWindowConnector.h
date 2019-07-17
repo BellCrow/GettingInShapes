c45602abf5aeed8333758e05ee813e6440fa9f07
Kim Kulling <kimkulling@users.noreply.github.com>
2019-07-17 13:06:01 +0200

    Update KeyboardWindowConnector.h
    Avoid copying.

--- KeyboardWindowConnector.h
+++ KeyboardWindowConnector.h
@@ -1,16 +1,23 @@
 #pragma once
+
 #include "Window.h"
 #include "Keyboard.h"
 
 class KeyboardWindowConnector
 {
-	Keyboard& m_keyboard; 
-	Window& m_window;
-
-	void HandleWindowMessage(const WindowMessage& message);
-
 public:
 	KeyboardWindowConnector(Keyboard& keyboard,Window& window);
 	~KeyboardWindowConnector();
+	
+        KeyboardWindowConnector( const KeyboardWindowConnector & rhs ) = delete;
+	KeyboardWindowConnector( KeyboardWindowConnector && rhs ) = delete;
+        KeyboardWindowConnector &operator = ( const KeyboardWindowConnector & rhs ) = delete;
+
+private:
+	void HandleWindowMessage(const WindowMessage& message);
+	
+private:
+	Keyboard& m_keyboard; 
+	Window& m_window;
 };
 
